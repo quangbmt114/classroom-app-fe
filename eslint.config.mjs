d44c2cd8518dbae473909f67a5d42f9bc15f1cc7
Quang nguyen <>
2025-07-12 16:44:11 +0700

    update rule eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
     },
   },
 ];
